51a10436faeed5c509056f519065b5ef94b45ff7
Virjanand <virjanand@virjanand.com>
2019-01-09 05:23:20 +0100

    Added text field and used data in logging

--- app/src/main/java/com/virjanand/a002_interactivitydemo/MainActivity.java
+++ app/src/main/java/com/virjanand/a002_interactivitydemo/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void doButtonAction(View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
         Log.i("Info", "Button was pressed!");
+        Log.i("Values", nameEditText.getText().toString());
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
